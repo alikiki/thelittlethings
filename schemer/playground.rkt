0e21cce8e5f58beb0e7f26e5220f823971ae5b46
Hye Woong Jeon <ajeon66@gmail.com>
2023-05-01 09:09:13 -0500

    added 75% of chapter 5

--- schemer/playground.rkt
+++ schemer/playground.rkt
@@ -169,3 +169,73 @@
   (cond
     ((one? n) (cdr lat))
     (else (cons (car lat) (rempick (sub1 n) (cdr lat))))))
+
+;CHAPTER 5 ----
+(define (rember* a l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l))
+     (cond
+       ((eq? (car l) a) (cdr l))
+       (else (cons (car l) (rember* a (cdr l))))))
+    (else (cons (rember* a (car l)) (rember* a (cdr l))))))
+
+(define (insertR* new old l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l))
+     (cond
+       ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l)))))
+       (else (cons (car l) (insertR* new old (cdr l))))))
+    (else (cons (insertR* new old (car l)) (insertR* new old (cdr l))))))
+
+(define (occur* a l)
+  (cond
+    ((null? l) 0)
+    ((atom? (car l))
+     (cond
+       ((eq? a (car l)) (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+    (else (+ (occur* a (car l)) (occur* a (cdr l))))))
+
+(define (subst* new old l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l))
+     (cond
+       ((eq? old (car l)) (cons new (subst* new old (cdr l))))
+       (else (cons (car l) (subst* new old (cdr l))))))
+    (else (cons (subst* new old (car l)) (subst* new old (cdr l))))))
+
+(define (insertL* new old l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l))
+     (cond
+       ((eq? old (car l)) (cons new (cons old (insertL* new old (cdr l)))))
+       (else (cons (car l) (insertL* new old (cdr l))))))
+    (else (cons (insertL* new old (car l)) (insertL* new old (cdr l))))))
+
+(define (member* a l)
+  (cond
+    ((null? l) #f)
+    ((atom? (car l))
+     (cond
+       ((eq? (car l) a) #t)
+       (else (member* a (cdr l)))))
+    (else (or (member* a (car l)) (member* a (cdr l))))))
+
+(define (leftmost l)
+  (cond
+    ((atom? (car l)) (car l))
+    (else (leftmost (car l)))))
+
+; (define (eqlist? l1 l2)
+;   (cond
+;     ((and (null? l1) (null? l2)) #t)
+;     ((or (null? l1) (null? l2)) #f)
+;     ()
+;     ((and (atom? l1) (atom? l2)) (eqan? l1 l2))
+;     ((or (atom? l1) (atom? l2)) #f)
+
+;     (else (and (eqan? l1 l2) (eqlist? (cdr l1) (cdr l2))))))
