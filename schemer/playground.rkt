3de7ce9eb926552efccebf6ef4bcaf2840170776
Hye Woong Jeon <ajeon66@gmail.com>
2023-05-01 11:20:08 -0500

    added two more ch5

--- schemer/playground.rkt
+++ schemer/playground.rkt
@@ -230,12 +230,16 @@
     ((atom? (car l)) (car l))
     (else (leftmost (car l)))))
 
-; (define (eqlist? l1 l2)
-;   (cond
-;     ((and (null? l1) (null? l2)) #t)
-;     ((or (null? l1) (null? l2)) #f)
-;     ()
-;     ((and (atom? l1) (atom? l2)) (eqan? l1 l2))
-;     ((or (atom? l1) (atom? l2)) #f)
+(define (eqlist? l1 l2)
+  (cond
+    ((and (null? l1) (null? l2)) #t)
+    ((or (null? l1) (null? l2)) #f)
+    ((and (atom? (car l1)) (atom? (car l2))) (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+    ((or (atom? (car l1)) (atom? (car l2))) #f)
+    (else (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))))
 
-;     (else (and (eqan? l1 l2) (eqlist? (cdr l1) (cdr l2))))))
+(define (equal? s1 s2)
+  (cond
+    ((and (atom? s1) (atom? s2)) (eqan? s1 s2))
+    ((or (atom? s1) (atom? s2)) #f)
+    (else (eqlist? s1 s2))))
